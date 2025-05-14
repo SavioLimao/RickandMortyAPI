f088df07e2cb9ae0c1acf92fda4158bf6c35c8ef
Savio Limao <saviolimao@hotmail.com>
2025-05-14 14:02:32 -0300

    PreparandoProps

---
+++ vite-project/src/Components/CRT.tsx
@@ -0,0 +1,31 @@
+export default function CRT(props) {
+  
+  
+    return (
+    <>
+      <div className="container">
+          <div className="CRTs">
+            <img className="ProfPic"
+              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+              alt=""
+            />
+            <section className="CRTinfos">
+              <h2 className="Name" id="Name">
+                {props.name}
+              </h2>
+              <hr />
+              <h3>Details:</h3>
+              <p><strong>Condition: </strong>
+              {props.status}
+              </p>
+              <p><strong>Species: </strong>
+              {props.species}
+              </p>
+              <p><strong>Origin: </strong>
+              {props.origin} (C-137)</p>
+            </section>
+          </div>
+      </div>
+    </>
+  );
+}
